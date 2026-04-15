1b2bc77c763cf97d06fb1db1eeb20e98e8b4acb6
Federico Jimenez <jimenezf95@gmail.com>
2026-04-15 17:19:34 -0500

    fix: base_url dinamica

--- utils/config_url.py
+++ utils/config_url.py
@@ -4,15 +4,18 @@
 # Claro-3899 
 #BASE_URL = "http://192.168.1.14:8501"
 
-# ACA BOGOTA
-#BASE_URL = "http://192.168.1.17:8501"
+ACA_BOGOTA = "http://192.168.1.17:8501"
 
 # CASA
 # BASE_URL = "http://192.168.1.10:8501" 
 
 
 # App desplegada Online (Render)
-BASE_URL = "https://sistema-mantenimiento-fpt8bvtkyaarj6c6q68jyo.streamlit.app/"
+#BASE_URL = "https://sistema-mantenimiento-fpt8bvtkyaarj6c6q68jyo.streamlit.app/"
 
+import streamlit as st
 
-#http://192.168.1.14:8501/?maquina_id=2
+BASE_URL = st.secrets.get(
+    "BASE_URL",
+    ACA_BOGOTA
+)
