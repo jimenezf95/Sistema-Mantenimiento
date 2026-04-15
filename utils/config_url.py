0dc54f81602c2ad84c694198d98918db78a22d76
Federico Jimenez <jimenezf95@gmail.com>
2026-04-15 16:50:47 -0500

    fix: actualizacion QR

--- utils/config_url.py
+++ utils/config_url.py
@@ -5,11 +5,14 @@
 #BASE_URL = "http://192.168.1.14:8501"
 
 # ACA BOGOTA
-BASE_URL = "http://192.168.1.17:8501"
+#BASE_URL = "http://192.168.1.17:8501"
 
 # CASA
 # BASE_URL = "http://192.168.1.10:8501" 
 
+
+# App desplegada Online (Render)
+BASE_URL = "https://sistema-mantenimiento-fpt8bvtkyaarj6c6q68jyo.streamlit.app/"
 
 
 #http://192.168.1.14:8501/?maquina_id=2
